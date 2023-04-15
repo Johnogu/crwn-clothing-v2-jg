111c977c5c74eb4c34f9095d8facdc3feda51bc1
Johnogu <130437401+Johnogu@users.noreply.github.com>
2023-04-15 19:48:42 +0200

    restructured code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "./categories.styles.scss";
-import "./components/category-item/category-item.component";
-import CategoryItem from "./components/category-item/category-item.component";
+//import "./categories.styles.scss";
+//import "./components/category-item/category-item.component";
+//import CategoryItem from "./components/category-item/category-item.component";
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
   const categories = [
@@ -31,13 +32,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
