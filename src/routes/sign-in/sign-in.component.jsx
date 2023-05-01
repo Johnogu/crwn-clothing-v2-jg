b91890906585af66558053115f9d4349802ae8b6
Johnogu <130437401+Johnogu@users.noreply.github.com>
2023-05-01 19:17:55 +0200

    lesson 97 redirect authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
+  //signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  /*     useEffect(() => {
+        (async () => {
+          const response = await getRedirectResult(auth);
+          if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+          }
+        })();
+      }, []); */
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -18,3 +32,7 @@ const SignIn = () => {
 };
 
 export default SignIn;
+
+// <button onClick={signInWithGoogleRedirect}>
+//Sign in with Google redirect
+//</button>
