c68065a83660ca2e48d879eee595f9c4fe441f0d
Johnogu <130437401+Johnogu@users.noreply.github.com>
2023-05-05 22:33:31 +0200

    lesson 100 signup with email and password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
   displayName: "",
@@ -13,6 +17,44 @@ const SignUpForm = () => {
 
   console.log(formFields);
 
+  const resetFormField = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    //check if pwd and confirm pwd matches
+    //if (!(password===confirmPassword){
+    if (password !== confirmPassword) {
+      //console.log("passwords does not match")
+      alert("passwords do not match");
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(user);
+      createUserDocumentFromAuth(user, { displayName });
+      setFormFields(defaultFormFields);
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("cannot create user, email already in use");
+      } else {
+        console.log("user creation encountered an error ", error);
+      }
+    }
+
+    //have we authenticated the user with email and pwd
+
+    //create user
+
+    //create userdocument with what was returned from "createAuthUser.."
+  };
+
   const handleChange = (event) => {
     const { name, value } = event.target;
 
@@ -22,7 +64,7 @@ const SignUpForm = () => {
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit}>
         <label>Display Name</label>
         <input
           type="text"
