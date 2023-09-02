126a235e0939915852d0bef3b089023daf1bd2e1
Johnogu <130437401+Johnogu@users.noreply.github.com>
2023-09-02 18:05:16 +0200

    created checkout page

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -6,8 +6,7 @@ import CheckoutItem from "../../components/checkout-item/checkout-item.component
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const { cartItems} =
-    useContext(CartContext);
+  const { cartItems, cartTotal } = useContext(CartContext);
 
   return (
     <div className="checkout-container">
@@ -29,11 +28,9 @@ const Checkout = () => {
         </div>
       </div>
       {cartItems.map((cartItem) => {
-        return (
-          <CheckoutItem key= {cartItem.id} cartItem={cartItem} />
-        );
+        return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
       })}
-      <span className="total">Total: 0</span>
+      <span className="total">Total: ${cartTotal}</span>
     </div>
   );
 };
