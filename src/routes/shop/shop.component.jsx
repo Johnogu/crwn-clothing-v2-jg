a8001af0d1453a1944b913dcdd9698c57b16128f
Johnogu <130437401+Johnogu@users.noreply.github.com>
2023-09-12 23:46:31 +0200

    lesson 134

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,21 @@
-import { useContext } from "react";
+/*import { useContext } from "react";
 
 import { CategoriesContext } from "../../contexts/categories.context";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
+*/
+import { Routes, Route } from "react-router-dom";
+
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 
 //import ProductCard from "../../components/product-card/product-card.component";
 import "./shop.styles.scss";
 //import SHOP_DATA from "../../shop-data.json";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 
